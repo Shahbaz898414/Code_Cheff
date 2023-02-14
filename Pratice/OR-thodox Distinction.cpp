f80b35170b6925b7027493411b6cd894bd4cbd45
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-02-14 16:20:12 +0530

    Add files via upload

---
+++ Pratice/OR-thodox Distinction.cpp
@@ -0,0 +1,180 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define Time cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+#define pb push_back
+#define mp make_pair
+#define line cout << endl;
+#define ff first
+#define ss second
+#define vi vector<int>
+#define no cout << "NO" << endl;
+#define yes cout << "YES" << endl;
+#define printv(v)                      \
+  for (int i = 0; i < (v.size()); i++) \
+  {                                    \
+    cout << v[i] << " ";               \
+  }                                    \
+  line;
+#define onesbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define sp(x, y) fixed << setprecision(y) << x
+#define w(x) \
+  int x;     \
+  cin >> x;  \
+  while (x--)
+#define tk(x) \
+  int x;      \
+  cin >> x;
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+#define debug(x)     \
+  cerr << #x << " "; \
+  _print(x);         \
+  cerr << endl;
+#else
+#define debug(x)
+#endif
+template <class T>
+void _print(T t)
+{
+  cerr << t;
+}
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+  cerr << "{";
+  _print(p.ff);
+  cerr << ",";
+  _print(p.ss);
+  cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+  cerr << "[ ";
+  for (T i : v)
+  {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T>
+void _print(vector<vector<T>> v)
+{
+  cerr << "[\n";
+  for (int l = 0; l < v.size(); l++)
+  {
+    {
+      for (int k = 0; k < v[l].size(); k++)
+        cerr << v[l][k] << " ";
+    }
+    cerr << "\n";
+  }
+  cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+  cerr << "[ ";
+  for (auto i : v)
+  {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+  cerr << "[ ";
+  for (T i : v)
+  {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+
+int add(ll a, ll b) {
+  return ((a % mod) + (b % mod)) % mod;
+}
+
+const long long inf = 1e18;
+const int MOD = 998244353;
+const int MAX = 1e6;
+
+bool isValid(string s) {
+  int len = s.size();
+  for (int i = 0; i < len / 2; i++) {
+    if (s[i] != s[len - 1 - i])
+      return false;
+  }
+  return true;
+}
+
+
+/*
+ll solve(ll n , ll k) {
+	if(n == 1 and k != 0) return 1;
+	if(n == 0) return 0;
+	if(dp[n][k] != -1) return dp[n][k];
+
+	ll ans = 0;
+	for(ll i = 0 ; i <= K ; i++) {
+		if(i != k) ans = (ans + solve(n - 1 , i)) % MOD;
+	}
+
+	return dp[n][k] = ans;
+}
+
+inline void getMeTheSolution() {
+  
+	cin >> N >> K;
+	ll n = N , k = K;
+
+	dp = vvll (N + 1 , vll (K + 1 , -1));
+	cout << solve(n , 0) nl;
+}*/
+
+
+
+int32_t main() {
+    fast
+    ll t;cin >> t;
+    while(t--) {
+        ll n; cin >> n;
+        ll a[n];
+        for (int i = 0; i < n; i++) {
+          cin>>a[i];
+        }
+        if(n>60) {
+          cout<<"NO"<<endl;
+          continue;
+        }
+
+        set<ll> s;
+
+        for(int i=0;i<n;i++){
+          ll k=0;
+          for(int j=i;j<n;j++) {
+            k=(a[j]|k);
+            s.insert(k);
+          }
+        }
+    
+      
+      if(s.size()==(int)n*(n+1)/2)
+        cout<<"YES\n";
+      else cout<<"NO\n";  
+        
+        
+    }
+  return 0;
+}
+
+
+
+
