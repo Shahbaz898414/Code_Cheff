5535ff0522007c98f8c3d3f21b392a20ab0f8a7b
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-03-14 15:51:56 +0530

    14-3-23

--- Pratice/Coloring the Grid.cpp
+++ Pratice/Coloring the Grid.cpp
@@ -229,9 +229,9 @@ int32_t main()
       // cout<<endl;
     }
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n-1; i++)
     {
-      for (int j = 0; j < m; j++)
+      for (int j = 0; j < m-1; j++)
       {
         if (
             (arr[i][j] == '1' || arr[i][j] == '2') && (arr[i][j + 1] == '1' || arr[i][j + 1] == '2') && (arr[i + 1][j] == '1' || arr[i + 1][j] == '2') && (arr[i + 1][j + 1] == '1' || arr[i + 1][j + 1] == '2'))
@@ -246,17 +246,13 @@ int32_t main()
 
     // ll cnt=0;
 
-    for (int i = 0; i < n; i++)
-    {
-      for (int j = 0; j < m; j++)
-      {
-       
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
         if(arr[i][j]=='1'){
           cnt=-1;
           break;
         }
       }
-      // cout<<endl;
       if(cnt==-1) break;
     }
 
