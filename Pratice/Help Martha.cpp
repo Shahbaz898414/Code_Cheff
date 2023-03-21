5605a14051ea2a7c6b53a361cc5b674170a2bdce
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-03-21 05:41:30 +0530

    2

--- Pratice/Help Martha.cpp
+++ Pratice/Help Martha.cpp
@@ -199,15 +199,7 @@ int32_t main()
   ll t;
   cin >> t;
   while (t--) {
-    int n, m;
-    cin >> n >> m;
-    char arr[n][m];
-
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        cin >> arr[i][j];
-      }
-    }
+    
   }
   return 0;
 }
