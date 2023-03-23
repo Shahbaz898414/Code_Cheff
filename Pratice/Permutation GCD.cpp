a2736a3dd582773dcaf4ec4507ea935dd583db3b
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-03-24 01:45:47 +0530

    1

--- Pratice/Permutation GCD.cpp
+++ Pratice/Permutation GCD.cpp
@@ -205,7 +205,7 @@ int32_t main()
 
     if(n>x)  cout<<-1;
     else {
-        cout<<x-(n)<<" ";
+      cout<<x-(n-1)<<" ";
       for(int i=1; i<=n; i++){
           if(i==(x-(n-1))) continue;
           cout<<i<<" ";
