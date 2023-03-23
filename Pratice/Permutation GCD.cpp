4adb2402b768e13816b53bb9d0b6ea73077bba6a
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-03-24 01:25:08 +0530

    1

--- Pratice/Permutation GCD.cpp
+++ Pratice/Permutation GCD.cpp
@@ -201,38 +201,8 @@ int32_t main()
   ll t;
   cin >> t;
   while (t--) {
-      long long x,y;
-	    cin>>x>>y;
-	    
-	    bool poss=false;
-	    for(long long i=1;i*i<=y;i++)
-	    {
-	        if(y%i==0)
-	        {
-	            long long a=i,b=y/i;
-	            if(x<=2*a-2)
-	            {
-	                poss=true;
-	                cout<<x-a+1<<" "<<a-1<<endl;
-	                cout<<a<<" "<<b<<endl;
-
-	                break;
-                  
-	            }
-	            else if(x>=2*b+2) {
-	                poss=true;
-	                cout<<a<<" "<<b<<endl;
-	                cout<<b+1<<" "<<x-b-1<<endl;
-	                break;
-	            }
-	        }
-	        
-	        
-	    }
-	    
-	    if(!poss) {
-	      cout<<-1<<endl;
-	    }
+     
+	   
         
   }
   return 0;
