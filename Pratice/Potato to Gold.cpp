d5c824820d606e8f28843f84802e6c4df62fdb1a
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-04-01 07:11:51 +0530

    1

--- Pratice/Potato to Gold.cpp
+++ Pratice/Potato to Gold.cpp
@@ -215,8 +215,28 @@ int32_t main()
     cin >> t;
     while (t--)
     {
-        int n, b, c;
-        cin >> n >> b;
+        int n, b, c,k;
+        cin >> n >>k;
+         for(auto &c:nums)cin>>c,c%=k;
+        vector<int> f(k);
+        for(int i = 0;i<n;i++){
+            f[nums[i]]++;
+        }
+        int ans = f[0]+1;
+        int sz = 0;
+        if(k&1){
+            sz = k/2;
+        }
+        else{
+            sz = (k/2)-1;
+            ans*=f[k/2]+1;
+        }
+        for(int i = 1;i<=sz;i++){
+            int x = ans;
+            ans =((x*(mod_pow(2,f[i])+mod_pow(2,f[k-i]))%mod))-x;
+            if(ans<0)ans+=mod;
+        }
+        cout<<ans<<endl;
        
     }
 }
