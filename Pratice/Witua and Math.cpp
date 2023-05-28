538f00b1f37137695324df32253cb19b43cd75fe
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-05-28 10:56:31 +0530

    1

--- Pratice/Witua and Math.cpp
+++ Pratice/Witua and Math.cpp
@@ -11,96 +11,96 @@ using namespace std;
 #define vi vector<int>
 #define no cout << "NO" << endl;
 #define yes cout << "YES" << endl;
-#define printv(v)                        \
-    for (int i = 0; i < (v.size()); i++) \
-    {                                    \
-        cout << v[i] << " ";             \
-    }                                    \
-    line;
+#define printv(v)                      \
+  for (int i = 0; i < (v.size()); i++) \
+  {                                    \
+    cout << v[i] << " ";               \
+  }                                    \
+  line;
 #define onesbits(x) __builtin_popcountll(x)
 #define zerobits(x) __builtin_ctzll(x)
 #define sp(x, y) fixed << setprecision(y) << x
-#define w(x)  \
-    int x;    \
-    cin >> x; \
-    while (x--)
+#define w(x) \
+  int x;     \
+  cin >> x;  \
+  while (x--)
 #define tk(x) \
-    int x;    \
-    cin >> x;
+  int x;      \
+  cin >> x;
 #define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 #ifndef ONLINE_JUDGE
-#define debug(x)       \
-    cerr << #x << " "; \
-    _print(x);         \
-    cerr << endl;
+#define debug(x)     \
+  cerr << #x << " "; \
+  _print(x);         \
+  cerr << endl;
 #else
 #define debug(x)
 #endif
 template <class T>
 void _print(T t)
 {
-    cerr << t;
+  cerr << t;
 }
 
 template <class T, class V>
 void _print(pair<T, V> p)
 {
-    cerr << "{";
-    _print(p.ff);
-    cerr << ",";
-    _print(p.ss);
-    cerr << "}";
+  cerr << "{";
+  _print(p.ff);
+  cerr << ",";
+  _print(p.ss);
+  cerr << "}";
 }
 
 template <class T>
 void _print(vector<T> v)
 {
-    cerr << "[ ";
-    for (T i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
+  cerr << "[ ";
+  for (T i : v)
+  {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
 }
 
 template <class T>
 void _print(vector<vector<T>> v)
 {
-    cerr << "[\n";
-    for (int l = 0; l < v.size(); l++)
+  cerr << "[\n";
+  for (int l = 0; l < v.size(); l++)
+  {
     {
-        {
-            for (int k = 0; k < v[l].size(); k++)
-                cerr << v[l][k] << " ";
-        }
-        cerr << "\n";
+      for (int k = 0; k < v[l].size(); k++)
+        cerr << v[l][k] << " ";
     }
-    cerr << "]";
+    cerr << "\n";
+  }
+  cerr << "]";
 }
 
 template <class T, class V>
 void _print(map<T, V> v)
 {
-    cerr << "[ ";
-    for (auto i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
+  cerr << "[ ";
+  for (auto i : v)
+  {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
 }
 
 template <class T>
 void _print(set<T> v)
 {
-    cerr << "[ ";
-    for (T i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
+  cerr << "[ ";
+  for (T i : v)
+  {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
 }
 
 const long long inf = 1e18;
@@ -109,35 +109,35 @@ const int MAX = 1e6;
 
 bool isValid(string s)
 {
-    int len = s.size();
-    for (int i = 0; i < len / 2; i++)
-    {
-        if (s[i] != s[len - 1 - i])
-            return false;
-    }
-    return true;
+  int len = s.size();
+  for (int i = 0; i < len / 2; i++)
+  {
+    if (s[i] != s[len - 1 - i])
+      return false;
+  }
+  return true;
 }
 
 void rotateMatrix(vector<vector<int>> &v, int n)
 {
-    for (int i = 0; i < n / 2; i++)
+  for (int i = 0; i < n / 2; i++)
+  {
+    for (int j = i; j < n - i - 1; j++)
     {
-        for (int j = i; j < n - i - 1; j++)
-        {
-            int ptr = v[i][j];
-            v[i][j] = v[n - 1 - j][i];
-            v[n - 1 - j][i] = v[n - 1 - i][n - 1 - j];
-            v[n - 1 - i][n - 1 - j] = v[j][n - 1 - i];
-            v[j][n - 1 - i] = ptr;
-        }
+      int ptr = v[i][j];
+      v[i][j] = v[n - 1 - j][i];
+      v[n - 1 - j][i] = v[n - 1 - i][n - 1 - j];
+      v[n - 1 - i][n - 1 - j] = v[j][n - 1 - i];
+      v[j][n - 1 - i] = ptr;
     }
+  }
 }
 
 int gcd(int a, int b)
 {
-    if (b == 0)
-        return a;
-    return gcd(b, a % b);
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
 }
 
 vector<bool> is_prime(10001, 1);
@@ -147,26 +147,29 @@ ll fact[1000001];
 
 ll power(ll a, ll b)
 {
-    ll ans = 1;
-    a %= mod;
-    while (b > 0)
-    {
-        if (b % 2)
-            ans = ans * a % mod;
-        a = a * a % mod;
-        b /= 2;
-    }
-    return ans;
+  ll ans = 1;
+  a %= mod;
+  while (b > 0)
+  {
+    if (b % 2)
+      ans = ans * a % mod;
+    a = a * a % mod;
+    b /= 2;
+  }
+  return ans;
 }
 
-int mod_pow(int a,int b){
-    int x = 1;
-    while(b){
-        if(b&1)(x*=a)%=mod;
-        (a*=a)%=mod;
-        b>>=1;
-    }
-    return x;
+int mod_pow(int a, int b)
+{
+  int x = 1;
+  while (b)
+  {
+    if (b & 1)
+      (x *= a) %= mod;
+    (a *= a) %= mod;
+    b >>= 1;
+  }
+  return x;
 }
 
 /*
@@ -183,30 +186,29 @@ int mod_pow(int a,int b){
   *      *
   *      *
   *      *
-  *      * * * * *    
+  *      * * * * *
 
 
 
 
 */
 
-
 void seive()
 {
-    is_prime[0] = 0;
-    is_prime[1] = 0;
-    for (int i = 2; i < 10001; i++)
-    {
+  is_prime[0] = 0;
+  is_prime[1] = 0;
+  for (int i = 2; i < 10001; i++)
+  {
 
-        if (is_prime[i])
-        {
-            primes.push_back(i);
-            for (int j = i + i; j < 10001; j += i)
-            {
-                is_prime[j] = 0;
-            }
-        }
+    if (is_prime[i])
+    {
+      primes.push_back(i);
+      for (int j = i + i; j < 10001; j += i)
+      {
+        is_prime[j] = 0;
+      }
     }
+  }
 }
 
 int cnt = 0;
@@ -214,105 +216,25 @@ vector<int> vis(100001);
 
 void dfs(vector<vector<int>> &adj, int i)
 {
-    vis[i] = 1;
-    cnt++;
-    for (auto nbr : adj[i])
+  vis[i] = 1;
+  cnt++;
+  for (auto nbr : adj[i])
+  {
+    if (vis[nbr] == 0)
     {
-        if (vis[nbr] == 0)
-        {
-            dfs(adj, nbr);
-        }
+      dfs(adj, nbr);
     }
+  }
 }
 
-int32_t main(){
-    ll T = 1,n,k;
-    cin>>T;
-    while(T--){
-        ll n, m;
+int32_t main()
+{
+  ll T = 1;
+  cin >> T;
+  while (T--)
+  {
 
-    cin >> n >> m;
-
-    ll fb[n];
-
-  
-    for(int i=0;i<n;i++) cin >> fb[i];
-   
-std::vector<pair<ll, ll>> nhg;
-
-     std::vector<pair<ll, ll>> bvgf;
-    sort(fb, fb + n);
-
-    
-   
-            
-    ll fbg[n + 1];
-               
-  
-    for(int i=0;i<n + 1;i++) fbg[i] = 0;
-    
-   
-            
-    while (m--)
-    {
-       ll a,b;
-                  
-    
-      cin >> a >> b;
-  
-            
-      fbg[a - 1] += 1;
-      fbg[b] -= 1;
-    }
-
-    for(int i=0;i<n + 1;i++) {
-
-      if (i>0)
- 
-
-        fbg[i] += fbg[i - 1];
-   
-            
-    }
-           
-  
-    
-    for(int i=0;i<n;i++)
-    {
-
-
-      bvgf.push_back({fbg[i], i});
-    }
-               
-    
-    sort(bvgf.rbegin(), bvgf.rend());
-
-    ll p = n - 1, ans = 0;
-               
-    
-    for (auto it : bvgf)
-    {
-      ans += (it.first * fb[p]);
-                 
-
-      nhg.push_back({it.second, fb[p--]});
-    }
-
-    cout << ans << "\n";
-    
-    
-    
-
-    sort(nhg.begin(), nhg.end());
-
-    for (auto it : nhg)
-     
-      cout << it.second << " ";
- 
-    
-    cout << endl;
-        
-              
-    }
-    
+    ll n;
+    cin >> n;
+  }
 }
