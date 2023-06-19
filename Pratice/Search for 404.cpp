1384a89992d7dc1d24e9345bbfddb8ce7e8566dd
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-19 22:24:13 +0530

    1

--- Pratice/Search for 404.cpp
+++ Pratice/Search for 404.cpp
@@ -161,20 +161,20 @@ ll bit(ll x, ll y) {
   return vnt;
 }
 
-// void rotateMatrix(vector<vector<int>> &v, int n)
-// {
-//   for (int i = 0; i < n / 2; i++)
-//   {
-//     for (int j = i; j < n - i - 1; j++)
-//     {
-//       int ptr = v[i][j];
-//       v[i][j] = v[n - 1 - j][i];
-//       v[n - 1 - j][i] = v[n - 1 - i][n - 1 - j];
-//       v[n - 1 - i][n - 1 - j] = v[j][n - 1 - i];
-//       v[j][n - 1 - i] = ptr;
-//     }
-//   }
-// }
+void rotateMatrix(vector<vector<int>> &v, int n)
+{
+  for (int i = 0; i < n / 2; i++)
+  {
+    for (int j = i; j < n - i - 1; j++)
+    {
+      int ptr = v[i][j];
+      v[i][j] = v[n - 1 - j][i];
+      v[n - 1 - j][i] = v[n - 1 - i][n - 1 - j];
+      v[n - 1 - i][n - 1 - j] = v[j][n - 1 - i];
+      v[j][n - 1 - i] = ptr;
+    }
+  }
+}
 
 signed main() {
 
