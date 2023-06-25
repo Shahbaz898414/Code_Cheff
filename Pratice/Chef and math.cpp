75484c87c315549f859bf483742395c7778bb4d8
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-26 02:52:56 +0530

    1

--- Pratice/Chef and math.cpp
+++ Pratice/Chef and math.cpp
@@ -176,34 +176,49 @@ void rotateMatrix(vector<vector<int>> &v, int n)
   }
 }
 
+
+ll f[55];
+ll x,k,q;
+ll solve(ll x,ll k, ll start) {
+  if(x==0 && k==0) {
+    return 1;
+  }
+  if(x==0 || k==0) {
+    return 0;
+  }
+  if(x<0) {
+    return 0;
+  }
+  if(x>f[start]*k) {
+    return 0;
+  }
+  return (solve(x-f[start],k-1,start)+solve(x,k,start-1))%mod;
+}
+
 signed main()
 {
 
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  vector<int> f;
 
-  f.push_back(1);
-  f.push_back(2);
-
-  int x = 1, y = 2;
-
-  while (y < mod)
-  {
-    int main = y;
-    x = x + y;
-    x = main;
-    f.push_back(y);
-  }
+  f[0]=1;
+f[1]=2;
+ll i=2;
+while(f[i-1]+f[i-2]<=1e9) {
+  f[i]=f[i-1]+f[i-2];
+  i++;
+}
+ 
+  ll start=i-1;
 
   ll t;
   cin >> t;
 
   while (t--)
   {
-    ll n, k;
-    cin >> n >> k;
+    cin>>x>>k;
+    cout<<solve(x,k,start)<<endl;
   }
 
   return 0;
