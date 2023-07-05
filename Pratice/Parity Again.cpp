eadee392a0df6df7810270ff30cb97c442e0974b
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-05 15:49:27 +0530

    1

--- Pratice/Parity Again.cpp
+++ Pratice/Parity Again.cpp
@@ -48,16 +48,56 @@ int main()
   cin >> t;
   while (t--)
   {
-    ll n;cin>>n;
+    ll n;
+    cin >> n;
 
     set<ll> s;
 
-    ll e=0,o=0;
+    ll e = 0, o = 0;
 
-    while(n--) {
-      ll x;cin>>x;
+    while (n--)
+    {
+      ll x;
+      cin >> x;
+
+      if (s.find(x) != s.end())
+      {
+        cout << e << " " << o << endl;
+        continue;
+      }
+
+      set<ll> ans;
+      set<ll>::iterator it;
+      for (it = s.begin(); it != s.end(); it++)
+      {
+        ll y = *it;
+        ll val = (x ^ y);
+        if (s.find(val) != s.end())
+          continue;
+        ll cnt = __builtin_popcount(val);
+        if (cnt % 2)
+          o++;
+        else
+          e++;
+
+        ans.insert(val);
+      }
+
+      for (it = ans.begin(); it != ans.end(); it++)
+      {
+        s.insert((*it));
+      }
+
+      s.insert(x);
+
+      ll cnt = __builtin_popcount(x);
+
+      if (cnt % 2)
+        o++;
+      else
+        e++;
+      cout << e << " " << o << "\n";
     }
-       
   }
   return 0;
 }
