2d5173756a45362d0f32c63cb9645f61bb43a409
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-02 22:00:56 +0530

    1

---
+++ Pratice/Square Sort.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define lld long long int
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  ll t;
+  cin >> t;
+  while (t--)
+  {
+    ll n, k;
+    cin >> n ;
+    vector<ll>  v(n);
+
+    for (ll i = 0; i < n; i++)
+    {
+      /* code */
+      cin>>v[i];
+    }
+    
+    
+  }
+
+  return 0;
+}
