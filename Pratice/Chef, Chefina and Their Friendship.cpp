17ac5a1d6d1ce659e2e81281123fe228b99ff1a1
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-02 22:09:12 +0530

    1

--- Pratice/Chef, Chefina and Their Friendship.cpp
+++ Pratice/Chef, Chefina and Their Friendship.cpp
@@ -3,7 +3,7 @@ using namespace std;
 typedef long long ll;
 #define lld long long int
 
-ll mx = 1e6 + 7, md = 1e9 + 7, bs = 25;
+ll mx = 1e6 + 7, md = 1e9 + 7, bs = 28;
 vector<ll> pw(mx), hs(mx);
 
 void build(string s)
@@ -30,39 +30,45 @@ int main()
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
-  //
+  
   ll t = 1, x = 1, i, j;
   pw[0] = 1;
   for (i = 1; i < mx; i++)
     pw[i] = (pw[i - 1] * bs) % md;
+
+  
   cin >> t;
-  while (t--)
-  {
+  while (t--) {
     ll i, n1, n, k, j = 0, m = 2e9, ct = 0, x = 0, y = 2e9, q;
     string s;
     cin >> s;
     n = s.size();
     build(s);
 
-    for (int i = 0; i <= n + 1; i++)
-    {
+    for (int i = 0; i <= n + 1; i++) {
       cout << hs[i] << " ";
     }
 
     cout << endl;
 
-    for (i = 1; i < n / 2; i++)
-    {
+
+     for (int i = 0; i <= n + 1; i++) {
+      cout << pw[i] << " ";
+    }
+
+    cout << endl;
+
+    for (i = 1; i < n / 2; i++) {
       j = n / 2 - i;
       x = gtHsh(0, i - 1);
       y = gtHsh(i, 2 * i - 1);
       k = gtHsh(2 * i, 2 * i + j - 1);
       q = gtHsh(2 * i + j, n - 1);
-      if (x == y && k == q)
-        ct++;
+      if (x == y && k == q) ct++;
     }
 
     cout << ct << '\n';
+
   }
 
   return 0;
