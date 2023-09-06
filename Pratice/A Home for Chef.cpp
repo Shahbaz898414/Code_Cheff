18fbff9c4cb3b12d83213ecbb862b7e54ee0e41a
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-09-06 17:45:32 +0530

    1

---
+++ Pratice/A Home for Chef.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define lld long long int
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  ll t;
+  cin >> t;
+  while (t--)
+  {
+    ll n;
+    cin >> n;
+
+    
+  }
+
+  return 0;
+}
