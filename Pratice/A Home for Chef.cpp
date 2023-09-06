692574f1325bfa95723953081038edc7524d337b
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-09-06 18:29:48 +0530

    1

--- Pratice/A Home for Chef.cpp
+++ Pratice/A Home for Chef.cpp
@@ -8,29 +8,37 @@ int main()
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
-  ll t; cin >> t;
+  ll t;
+  cin >> t;
 
-  while (t--) {
-     int n; cin >> n;
+  while (t--)
+  {
+    int n;
+    cin >> n;
     vector<int> xs(n), ys(n);
-    for(int i=0;i<n;++i) {
-        cin >> xs[i] >> ys[i];
-    }
+    for (int i = 0; i < n; ++i)
+      cin >> xs[i] >> ys[i];
     
+
     sort(xs.begin(), xs.end());
+
+    
     sort(ys.begin(), ys.end());
-    
+
     int ans;
-    if ((n&1)) {
-        int mid = n/2;
-        ans = 1;
-    } else {
-        int mid = (n+1)/2;
-        ans = abs(xs[mid] - xs[mid-1] + 1) * abs(ys[mid] - ys[mid-1] + 1);
+
+    if ((n & 1))
+    {
+      int mid = n / 2;
+      ans = 1;
     }
-    
+    else
+    {
+      int mid = (n + 1) / 2;
+      ans = abs(xs[mid] - xs[mid - 1] + 1) * abs(ys[mid] - ys[mid - 1] + 1);
+    }
+
     cout << ans << '\n';
-    
 
 
 
