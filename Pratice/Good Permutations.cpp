f06c402711982ee7213bb8aa6817aba0a2c3842c
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-09-23 23:23:52 +0530

    1

--- Pratice/Good Permutations.cpp
+++ Pratice/Good Permutations.cpp
@@ -215,9 +215,38 @@ bool check(int GCD, int K)
 
 void solve()
 {
-  
-
-  
+  int n, k;
+  cin >> n >> k;
+  vector<int> v(n);
+  for (int i = 0; i < n; i++)
+  {
+    cin >> v[i];
+  }
+  vector<int> p(n);
+  for (int i = 0; i < n; i++)
+    p[i] = i + 1;
+  int ans = 0;
+  do
+  {
+    int cnt = 0;
+    for (int i = 1; i < n; i++)
+    {
+      if (p[i] > p[i - 1])
+        cnt++;
+    }
+    int flag = 0;
+    for (int i = 0; i < n; i++)
+    {
+      if (v[i] != 0 and p[i] != v[i])
+      {
+        flag = 1;
+        break;
+      }
+    }
+    if (cnt == k and flag == 0)
+      ans++;
+  } while (next_permutation(p.begin(), p.end()));
+  cout << ans << endl;
 }
 
 int32_t main()
@@ -234,7 +263,6 @@ int32_t main()
 
   while (t--)
   {
-
     solve();
   }
 }
