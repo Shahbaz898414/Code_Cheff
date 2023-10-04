aa9f867d01fb6a5624fac6428b95ddfaee31289b
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-10-05 01:03:04 +0530

    1

---
+++ Pratice/Chef Segment.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+
+int main()
+{
+  int t;
+  cin >> t;
+
+  while (t--)
+  {
+    ll int n;
+    
+
+   vector<ll> arr(n);
+
+   for (ll i = 0; i < n; i++)
+   {
+    /* code */
+    cin>>arr[i];
+   }
+   
+
+  }
+  return 0;
+}
