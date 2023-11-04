378bc0c6ce8d01a496f027bac496bbdadccb514f
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-11-04 19:25:23 +0530

    1

---
+++ Pratice/Matchsticks.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+
+int main()  {
+
+
+  int t; cin >> t;
+
+  while (t--) {
+    ll n;cin>>n;
+
+    vector<ll>  ar(n);
+
+    for (ll i = 0; i < n; i++)
+    {
+      /* code */
+      cin>>ar[i];
+    }
+
+    ll q;cin>>q;
+       
+
+   
+
+  }
+  return 0;
+}
+
+
+
+
+
