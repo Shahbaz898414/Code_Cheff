3728a8b51c6fa3ffdd0cf19f1777204e84ca5fca
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-11-23 14:49:44 +0530

    1

---
+++ Pratice/Many Chefs.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+
+
+int main()
+{
+    int T = 1;cin>>T;
+    while (T--) {
+      string s;cin>>s;
+
+      ll len=s.size();
+
+
+             
+
+    }
+
+    
+
+    return 0;
+}
+
+
+
+
+/*
+
+
+Dear Sir,
+
+I hope this message finds you well. I wanted to express my enthusiasm and let you know that I have completed the assigned task for the intern/job role application. It was a rewarding experience, and I enjoyed the opportunity to showcase my skills and approach to the given assignment.
+
+I am eager to discuss my submission in further detail and explore how my capabilities align with the requirements of the role. I am available at your convenience for an interview to delve deeper into my qualifications and learn more about the team and company.
+
+
+
+Thank you for considering my application. I look forward to the possibility of discussing how my skills and experiences make me a strong fit for this position.
+
+Best Regards,
+Shahbaz khan
+7710968756
+
+*/
+
+
+
+/*
+
+
+Shaunak Jagtap
+
+
+
+*/
--- Pratice/Pokemon Battles.cpp
+++ Pratice/Pokemon Battles.cpp
@@ -36,11 +36,51 @@ int main()
 
        sort((v).begin(),(v).end());
 
-       
-      
+      //  for(auto it:v){
+      //   cout<<it.first<<" "<<it.second<<endl;
+      //  }
 
-      
-      
+
+
+      //  cout<<endl;
+
+      //  for (int i = n-1; i >=0 ; i--)
+      //  {
+      //   /* code */
+      //   cout<<v[i].first<<" "<<v[i].second<<endl;
+      //  }
+
+
+      //  cout<<endl;
+      //  cout<<endl;
+
+       
+
+
+        int ans = 0, mx = 0;
+
+
+        for (int i = n - 1; i >= 0; i--) {
+          
+
+            if (v[i].second > mx) {
+                ans++;
+            }
+
+            mx = max(mx, v[i].second);
+            // cout<<mx<<" "<<v[i].second<<endl;
+
+        }
+
+
+
+        // cout<<endl;
+
+
+
+        cout << ans << endl;
+
+             
 
     }
 
@@ -49,3 +89,34 @@ int main()
     return 0;
 }
 
+
+
+
+/*
+
+There are N Pokemon trainers numbered from 1 to N. Each trainer has one Pokemon. All the trainers have arrived to participate in a contest. There are two battle arenas, one in ground and other in water. Each Pokemon may have different power in both the arenas. When two Pokemon fight in an arena, the Pokemon having higher power in that arena wins. It is guaranteed that all Pokemon have distinct powers in the same arena to avoid any ties.  The strength of a Pokemon trainer is determined to be the number of other Pokemon his Pokemon can defeat in at least one arena.  It is known that only the Pokemon trainers with the highest strength will qualify for the next round (multiple trainers may have the same strength). Find the number of Pokemon trainers who will qualify for the next round.  
+
+Input
+Format The first line of input will contain a single integer T, denoting the number of test cases. Each test case consists of three lines of input. The first line of each test case contains a single integer N, denoting the number of Pokemon trainers. The second line will contain N space-separated integers A1 ,A2 ,…,A  N ​  , where Ai ​denotes the power of the Pokemon of  ith   trainer in the ground arena. The third line will contain N space-separated integers,B1,B2 ,…,B  N ​  , where Bi denotes the power of the Pokemon of  ith   trainer in the water arena. 
+
+Output 
+Format For each test case, output on a new line the number Pokemon trainers who will qualify for the next round.
+
+
+
+
+
+Test case 4: The maximum strength possible is 4. There are 3 trainers with strength  4:
+
+Trainer 3: Trainer  3's pokemon has ground power equal to  5, so, it can defeat all other  4 pokemons in the ground arena. Thus, the trainer's strength is 4.
+
+Trainer  4: Trainer  4's pokemon has ground power equal to 4. It can beat the pokemons having ground power less than 4. These belong to trainers 1,2, and  5. Similarly, it has water power equal to 5. Thus, it can also beat trainer 3's pokemon which has water power  1 1. Thus, total strength of this trainer is  4.
+
+Trainer  5: Trainer  5's pokemon has water power equal to  6, so, it can defeat all other 4 pokemons in the water arena. Thus, the trainer's strength is  4.
+
+
+5
+2 3 5 4 1
+4 2 1 5 6
+
+*/
