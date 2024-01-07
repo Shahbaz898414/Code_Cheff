839e9e72c9256fa080b2c5fd4b45ddcaa913241a
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-01-07 16:48:44 +0530

    1

--- Pratice/A PLUS B Remastered.cpp
+++ Pratice/A PLUS B Remastered.cpp
@@ -13,38 +13,63 @@ int main()
   while (t--)
   {
 
-    ll n, x, y; cin >> n;
+    ll n, x, y;
+    cin >> n;
 
-    vector<ll>  arr(n);
+    vector<ll> arr(n);
 
     vector<ll> brr(n);
 
     for (ll i = 0; i < n; i++)
     {
-      
-      cin>>arr[i];
+
+      cin >> arr[i];
     }
 
     for (ll i = 0; i < n; i++)
     {
-      
-      cin>>brr[i];
+
+      cin >> brr[i];
     }
+
+    sort(arr.begin(), arr.end());
+    sort(brr.begin(), brr.end(), greater<int>());
+
+    bool fl=true;
 
     for (int i = 0; i < n; i++)
     {
       /* code */
-      for (int j = 0; j < count; j++)
+      if (arr[i] + brr[i] != arr[0] + brr[0])
+      {
+        fl=false;
+        break;
+      }
+    }
+
+
+
+    if(!fl){
+      cout<<-1<<endl;
+    }else{
+      for (int i = 0; i < n; i++)
       {
         /* code */
+        cout<<arr[i]<<" ";
       }
+
+      cout<<endl;
+
+      for (int i = 0; i < n; i++)
+      {
+        /* code */
+        cout<<brr[i]<<" ";
+      }
+
+      cout<<endl;
+      
       
     }
-    
-    
-    
-
-   
   }
   return 0;
 }
