59ce65a90be5e61d0f7afaeffa949cd1be5fefdb
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-01-07 16:50:58 +0530

    1

---
+++ Pratice/Greedy LIS Algorithm.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+const int N = 1e5 + 5;
+
+int main()
+{
+
+  int t;
+  cin >> t;
+
+  while (t--)
+  {
+
+    ll n, x, y;
+    cin >> n>>x;
+
+    
+  }
+  return 0;
+}
