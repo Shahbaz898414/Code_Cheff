6c81f02929e16767f710405709a7184028306c46
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-01-08 21:11:19 +0530

    1

--- Pratice/Greedy LIS Algorithm.cpp
+++ Pratice/Greedy LIS Algorithm.cpp
@@ -16,7 +16,7 @@ int main()
     ll n, x, y;
     cin >> n>>x;
 
-    
+
 
     
   }
@@ -25,60 +25,3 @@ int main()
 
 
 
-/*
-
-
-Hello,
-
-I genuinely would like to say Thanks to you guys for showing interest in working with us. !
-
-Currently, we are working on a very exciting Project - the "Sellerkin" SAAS Tool - The purpose of this tool is 
-
-to help e-commerce sellers to find the winning products and keyword volume.
-
-Sellerkin Tech Stack - MERN is Useds, so, whatever you create, please create acc. to this tech stack in mind 
-
-There are 2 Tasks, you have to do any one task from these two:- 
-
-1. You have to create a Sales Feature for listings available for the Product finder feature for Sellerkin
-
-Sales data can't be fetched directly from Etsy APIs because there is no endpoint in Etsy documentation to fetch that data
-
-But still, if you would like to use Etsy APIs to fetch listings or maybe need to build an algorithm using views or favorites, if that is the case, 
-
-please share your approach at sellerkin@gmail.com and we'll share API keys with you
-
-To understand more about the sales feature, you can check alura.io, everbee.io and check sales feature of it to understand in better way
-
-You can signup on to these websites for a free trial 
-
-Note: - Only work on sales feature no need to fetch other results of Product Finder 
-
-2. You need to create a Keyword Search Volume feature for the Etsy.com platform 
-
-Keyword search volume - It simply means how many times that specific keyword searched per monthly basis on etsy.com 
-
-for example - if someone searches " Ring " and like to know how much the keyword search volume for " Ring "
-
-on etsy.com then we need to show the more expected keyword search volume - it could be 10000 or 8762 or anything - this is the feature 
-
-we need to create. 
-
-To understand more about the market - You can check out the tools - alura.io, etsyhunt, everbee.io - how keyword  search volume displayed 
-
-on these tools 
-
-Once you do any task, please deploy it on render or vercel, it's free and send it to us at sellerkin@gmail.com
-
-Then we'll have a meet for a final call 
-
-3. You must know how to crawl any web pages ( links or images )
-
-If not understood properly, please read it multiple times still you feel we left something, please feel free to ask. 
-
-Internship Period - We are looking for an intern for at least 2 months 
-
-
-
-
-*/
