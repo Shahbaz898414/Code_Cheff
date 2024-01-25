216518e327ff85f132ff2756c5f45073a6f5b4c0
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-01-26 00:43:56 +0530

    1

--- Pratice/Binary Grid Forces.cpp
+++ Pratice/Binary Grid Forces.cpp
@@ -1,111 +1,101 @@
-#include <bits/stdc++.h>
-
+#include "bits/stdc++.h"
 using namespace std;
-#define ll long long
 
-const int mod = 1e9 + 7;
-// const int N = 1e5 + 5;
+#define int long long int
+#define double long double
+#define endl '\n'
 
-const int N = 1e6 + 2;
-int pow1(int x, int y)
-{
-  int res = 1;
-  x = x % mod;
-  if (x == 0)
-    return 0;
-  while (y > 0)
-  {
-    if (y & 1)
-      res = (res * x) % mod;
-    y = y >> 1;
-    x = (x * x) % mod;
-  }
-  return res;
+const int MOD = 1000000007;
+
+int modInverse(int A, int M = MOD) {
+    int m0 = M;
+    int y = 0, x = 1;
+    if (M == 1)
+        return 0;
+    while (A > 1) {
+        int q = A / M;
+        int t = M;
+        M = A % M, A = t;
+        t = y;
+        y = x - q * y;
+        x = t;
+    }
+    if (x < 0)
+        x += m0;
+    return x;
 }
 
-int f[N], inverse[N];
-int divide(int n)
-{
-  return pow1(n, mod - 2);
-}
+void solve(){
+    int n,m;
+    cin>>n>>m;
 
-int ncr(int n, int r)
-{
-  if (n < r)
-    return 0;
-  return (f[n] * ((divide(f[r]) * divide(f[n - r])) % mod)) % mod;
-}
+    
+    string s[n];
 
-int main()
-{
-
-  int t;
-  cin >> t;
-
-  while (t--)
-  {
-
-    int n, m;
-    cin >> n >> m;
-
-    vector<string> v1(n);
-
-    for (int i = 0; i < n; i++)
-    {
-      /* code */
-      cin >> v1[i];
+    for (int i = 0; i < n; i++) {
+        cin>>s[i];
     }
 
-    set<int> curr;
+    vector<vector<bool>> dp(n,vector<bool>(m,false));
 
-    int cnt1 = 0, cnt2 = 0;
-
-    for (int i = n - 1; i >= 0; i--)
-    {
-      /* code */
-      cnt1 = 0;
-      cnt2 = 0;
-
-      for (auto it : curr)
-      {
-        if (v1[i][it] == '1')
-        {
-          cnt2++;
-          curr.insert(it);
+    if (n==1){
+        cout<<1<<endl;
+        return;
+    }
+    for (int i = 0; i < m; i++) {
+        if (s[n-1][i]=='1') dp[n-1][i]=true;
+    }
+    for (int i = n - 2; i >= 0; i--) {
+        int ones = 0;
+        for (int j = 0; j < m; j++) {
+            if (s[i][j]=='1') ones++;
         }
-      }
-
-      if (cnt1 == 0)
-        curr.clear();
-
-      for (int j = 0; j < v1[i].size(); j++)
-      {
-        if (v1[i][j] == '1')
-        {
-          cnt2++;
-          curr.insert(j);
+        if (ones<2) continue;
+        bool check =false;
+        for (int j = 0; j < m; j++) {
+            if (s[i][j]=='1' && dp[i+1][j]){
+                check = true;
+            }
         }
-      }
-
-      if (cnt2 <= 1)
-      {
-        curr.clear();
-      }
+        for (int j = 0; j < m; j++) {
+            if (check && dp[i+1][j]){
+                dp[i][j]=true;                
+            } else if (s[i][j]=='1') dp[i][j]=true;
+        }
     }
-
-    int dj1 = 0;
-    for (auto it : curr)
-    {
-      if (v1[0][it] == '1')
-        dj1++;
+    int count_0=0, count_1=0;
+    for (int i = 0; i < m; i++) {
+        if (dp[1][i]){
+            if (s[0][i]=='1') count_1++;
+            else count_0++;
+        }
     }
-    int dj2 = (curr.size() - dj1);
-    int ans = (ncr(dj1 + dj2, dj1));
-    // int ans=(f1(dj1+dj2) /((f1(dj1)*f1(dj2))));
-    cout << ans << endl;
-  }
-  return 0;
+    int ans = 1;
+    for (int i = 1; i <= count_0+count_1; i++) {
+        ans = ans*i;
+        ans = ans%MOD;
+    }
+    for (int i = 1; i <= count_0; i++) {
+        ans = ans * modInverse(i);
+        ans = ans%MOD;
+    }
+    for (int i = 1; i <=count_1; i++) {
+        ans = ans * modInverse(i);
+        ans = ans%MOD;
+    }
+    cout<<ans<<endl;
 }
 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
 
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
 
+    return 0;
+}
