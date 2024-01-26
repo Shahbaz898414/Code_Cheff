c350b3df8305011b532a3889c9ff33a78fea0866
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-01-27 01:15:58 +0530

    1

--- Pratice/Chef Product.cpp
+++ Pratice/Chef Product.cpp
@@ -7,48 +7,36 @@ using namespace std;
 
 const int MOD = 1000000007;
 
-int modInverse(int A, int M = MOD)
-{
-  int m0 = M;
-  int y = 0, x = 1;
-  if (M == 1)
-    return 0;
-  while (A > 1)
-  {
-    int q = A / M;
-    int t = M;
-    M = A % M, A = t;
-    t = y;
-    y = x - q * y;
-    x = t;
-  }
-  if (x < 0)
-    x += m0;
-  return x;
-}
 
-void solve()
-{
 
-  int n;
-  cin >> n;
+
+void solve() {
+
+
+  int n; cin >> n;
+
 
   int x = sqrtl(n);
 
-  if (n == 2)
-  {
+
+  if (n == 2) {
     cout << 0 << endl;
     return;
   }
 
-  if (n % 2)
-  {
+
+  if (n % 2) {
     cout << (x + 1) / 2 << endl;
     return;
   }
 
+
   cout << x / 2 << endl;
+
+
 }
+
+
 
 signed main()
 {
@@ -65,3 +53,6 @@ signed main()
 
   return 0;
 }
+
+
+
