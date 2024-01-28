39e6d87cc71bc3dab850c8c1af5aa31c477c922f
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-01-28 22:52:21 +0530

    1

---
+++ Pratice/Christmas Candy.cpp
@@ -0,0 +1,58 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define ll long long int
+#define double long double
+#define endl '\n'
+
+const int MOD = 1000000007;
+
+const int inf = 1ll << 60;
+
+int modInverse(int A, int M = MOD)
+{
+  int m0 = M;
+  int y = 0, x = 1;
+  if (M == 1)
+    return 0;
+  while (A > 1)
+  {
+    int q = A / M;
+    int t = M;
+    M = A % M, A = t;
+    t = y;
+    y = x - q * y;
+    x = t;
+  }
+  if (x < 0)
+    x += m0;
+  return x;
+}
+
+void solve()
+{
+
+  int n;
+	    cin >> n;
+	    
+
+
+
+}
+
+signed main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t = 1;
+  cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+
+  return 0;
+}
+
