461c6c45651ef8f0eea397430cfbae66f65f1fe2
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-01-31 16:23:19 +0530

    1

--- Pratice/Game on Array.cpp
+++ Pratice/Game on Array.cpp
@@ -29,48 +29,68 @@ int modInverse(int A, int M = MOD)
   return x;
 }
 
-void solve()
-{
 
-  int n;
-  cin >> n;
-  string s;
-  cin >> s;
-  int to_win = n / 2;
-  to_win++;
-  int p = n - to_win;
-  string ans = "";
-  for (auto &b : s)
-  {
-    if (b == 'S')
-    {
-      ans += 'R';
-    }
-    else if (b == 'R')
-    {
-      ans += 'P';
-    }
-    else
-    {
-      ans += 'S';
-    }
+void solve() {
+
+
+  int n; cin >> n;
+
+
+  vector<int> arr(n);
+
+  int cnt1=0,cnt2=0;
+
+
+  for (int i = 0; i < n; i++) {
+    cin>>arr[i];
+    if(i%2) cnt2+=arr[i];
+    else cnt1+=arr[i];
+
   }
 
+
+  if(n==1){
+    if((cnt1+cnt2)>1){
+      cout<<"Alice"<<endl;
+      return;
+    }
+
+    cout<<"Bob"<<endl;
+    return;
+  }
+
+
+  if((cnt1+cnt2)%2!=0){
+    cout<<"Bob"<<endl;
+    return;
+  }
+
+  if(cnt1%2 == 0){
+		int a1=((cnt1+cnt2-2)/2);
+		
+		if(a1%2 != 0){
+			cout<<"Bob"<<endl;
+			return;
+		}
+		cout<<"Alice"<<endl;
+		return;
+	}
+	else{
+		
+		int a1=((cnt1+cnt2-2)/2);
+		
+		if(a1%2 == 0){
+			cout<<"Bob"<<endl;
+			return;
+		}
+		cout<<"Alice"<<endl;
+		return;
+	}
   
-  for (int i = 0; i < ans.size(); i++)
-  {
-    if (p == 0)
-    {
-      break;
-    }
-    if (ans[i] == 'R' or ans[i] == 'S')
-    {
-      ans[i] = 'P';
-      p--;
-    }
-  }
-  cout << ans << "\n";
+ 
 }
+
+
 
 signed main()
 {
@@ -87,25 +107,3 @@ signed main()
 
   return 0;
 }
-
-
-
-
-/*
-
-
-Today was my 13th day out of the 75 days hard challenge.
-So today. I solved 5 question.
-
-1. Rock-Paper-Scissors (https://www.codechef.com/problems/MINRPC).
-
-2. Christmas Candy (https://www.codechef.com/problems/CHRISCANDY).
-
-3. Exact Savings (https://www.codechef.com/problems/EXACTSAVINGS)
-
-4. 1074. Number of Submatrices That Sum to Target (https://leetcode.com/problems/number-of-submatrices-that-sum-to-target/description/?envType=daily-question&envId=2024-01-28).
-
-
-5. Am I Lucky! (https://www.codechef.com/problems/SPCP4?tab=statement).
-
-*/
