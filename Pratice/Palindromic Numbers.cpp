0fb7fe72b63aaa8a56e2d8b8e3d011749f0fddb2
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-02-06 03:50:34 +0530

    1

--- Pratice/Palindromic Numbers.cpp
+++ Pratice/Palindromic Numbers.cpp
@@ -26,17 +26,27 @@ int main()
       continue;
     }
 
+
+
     long long base = 2;
+
+
     bool flag = 0;
+
+
     long long tt;
+
+
     while (1LL * base * base <= n)
     {
+
       tt = n;
       if (!(tt % base))
       {
         ++base;
         continue;
       }
+
       long long left = tt / base;
       long long right = tt - base * left;
       long long q;
@@ -72,75 +82,3 @@ int main()
 }
 
 
-
-
-
-
-/*
-
-#include<cstdio>
-int main()
-{
-  int t;
-  long long n;
-  int base;
-  scanf("%d",&t);
-  while(t--)
-    {
-        scanf("%lld",&n);
-        if(n==1||n==3)
-        {
-            printf("2\n");
-            continue;
-        }
-        if(n==2||n==4)
-        {
-            printf("3\n");
-            continue;
-        }
-        base = 2;
-        bool flag = 0;
-        long long tt ;
-        while(1LL*base*base<=n)
-        {
-            tt = n;
-            if(!(tt%base))
-            {
-              ++base;
-              continue;
-      }
-            long long left = tt/base;
-            long long right = tt - base*left;
-            long long q;
-            if(right==0)
-            {
-              ++base;
-              continue;
-      }
-            for(q=left/base;q>right;q=left/base)
-            {
-              right = base *(right-q) + left;
-              left = q;
-      }
-      if(left==right||q==right)
-      {
-        flag = 1;
-        printf("%d\n",base);
-        break;
-      }
-            ++base;
-        }
-        if(!flag)
-        {
-            base--;
-            long long p;
-            while((p=n/base)==base+1||base*p!=n)
-                base--;
-            printf("%lld\n",p-1);
-        }
-    }
-  return 0;
-}
-
-
-*/
