d4d92f931145853e74c91f92f4247abe62c3f84b
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-02-11 02:15:42 +0530

    1

--- Pratice/zzz1.cpp
+++ Pratice/zzz1.cpp
@@ -3,61 +3,69 @@
 #define int long long
 using namespace std;
 int n, m, x, i, j;
-signed main() {
-    // your code goes here
-    int t;
-    cin >> t;
-    while (t--) {
-        vector < int > p, q, r, s;
-        cin >> n >> m;
-        if (n == 1 || m == 1) {
-            for (i = 0; i < n; i++)
+signed main()
+{
+	// your code goes here
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		vector<int> p, q, r, s;
+		cin >> n >> m;
+		if (n == 1 || m == 1)
+		{
+			for (i = 0; i < n; i++)
 
-                for (j = 0; j < m; j++) {
-                    cin >> x;
-                    p.push_back(x);
-                }
+				for (j = 0; j < m; j++)
+				{
+					cin >> x;
+					p.push_back(x);
+				}
 
-            for (i = 0; i < n; i++)
+			for (i = 0; i < n; i++)
+				for (j = 0; j < m; j++)
+				{
+					cin >> x;
+					q.push_back(x);
+				}
 
-                for (j = 0; j < m; j++) {
-                    cin >> x;
-                    q.push_back(x);
-                }
-            if (p == q)
-                puts("yes");
-            else
-                puts("no");
+			if (p == q)
+				puts("yes");
+			else
+				puts("no");
+		}
+		else
+		{
+			for (i = 0; i < n; i++)
 
-        } else {
-            for (i = 0; i < n; i++)
+				for (j = 0; j < m; j++)
+				{
+					cin >> x;
+					if ((i ^ j) % 2)
+						p.push_back(x);
+					else
+						q.push_back(x);
+				}
+			for (i = 0; i < n; i++)
 
-                for (j = 0; j < m; j++) {
-                    cin >> x;
-                    if ((i ^ j) % 2)
-                        p.push_back(x);
-                    else
-                        q.push_back(x);
-                }
-            for (i = 0; i < n; i++)
+				for (j = 0; j < m; j++)
+				{
+					cin >> x;
 
-                for (j = 0; j < m; j++) {
-                    cin >> x;
-
-                    if ((i ^ j) % 2)
-                        r.push_back(x);
-                    else
-                        s.push_back(x);
-                }
-            sort(p.begin(), p.end());
-            sort(q.begin(), q.end());
-            sort(r.begin(), r.end());
-            sort(s.begin(), s.end());
-            if (p == r && q == s)
-                puts("yes");
-            else
-                puts("no");
-        }
-    }
-    return 0;
+					if ((i ^ j) % 2)
+						r.push_back(x);
+					else
+						s.push_back(x);
+				}
+			sort(p.begin(), p.end());
+			sort(q.begin(), q.end());
+			sort(r.begin(), r.end());
+			sort(s.begin(), s.end());
+			if (p == r && q == s)
+				puts("yes");
+			else
+				puts("no");
+		}
+	}
+	return 0;
 }
