10d0cf3648fb96e77ac0234d7faeb08d6a33c101
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-02-14 00:48:15 +0530

    1

--- Pratice/Chef and Water Car.cpp
+++ Pratice/Chef and Water Car.cpp
@@ -6,8 +6,6 @@ using namespace std;
 // #define MOD 998244353
 const int m = 1e9 + 7;
 
-
-
 ll binexp(ll a, ll b, ll m)
 {
   ll result = 1;
@@ -23,45 +21,37 @@ ll binexp(ll a, ll b, ll m)
   return result;
 }
 
-
 #define MAX 100010
-
 
 ll a[MAX], b[MAX];
 
-
 int length[MAX];
-
-
 
 int main()
 {
   int t;
   cin >> t;
 
-  while (t--) {
+  while (t--)
+  {
 
+    ll n = 0, v = 0;
+    cin >> n >> v;
 
-    ll n, w;cin >> n >> w;
+    ll p = n - 1, r = p - v, c = 1;
+    ll maxi = p * (p + 1) / 2;
+    ll mini = 0;
 
+    if (v >= n - 1)
+      mini = n - 1;
+    else
+    {
+      mini = v - 1;
+      mini += (p - v + 1) * (p - v + 2) / 2;
+    }
 
-
-  
-
-
+    cout << maxi << " " << mini << endl;
   }
-
 
   return 0;
 }
-
-
-
-/*
-
-vector<vector<int>> ans;
-
-
-
-
-*/
