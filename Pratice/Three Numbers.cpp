bad6f643b8c694c27e709cfd475ebd0d18085660
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-02-18 01:16:25 +0530

    1

--- Pratice/Three Numbers.cpp
+++ Pratice/Three Numbers.cpp
@@ -191,19 +191,20 @@ void solution()
 {
 
   int a[3];
-  for (int i = 0; i < 3; i++)
-    cin >> a[i];
+
+  for (int i = 0; i < 3; i++) cin >> a[i];
   
-  
-  if (abs(a[0] - a[1]) % 2 == 1 || abs(a[1] - a[2]) % 2 == 1 || abs(a[2] - a[0]) % 2 == 1)
-  {
+
+  if(abs(a[0] - a[1]) % 2 == 1 || abs(a[1] - a[2]) % 2 == 1 || abs(a[2] - a[0]) % 2 == 1) {
     cout << -1;
   }
-  else
-  {
+  else {
+
     sort(a, a + 3);
     int avg = (a[1] + a[2]) / 2;
+    // cout<<avg<<endl;
     cout << avg - a[0];
+
   }
   cout << "\n";
 
@@ -223,3 +224,6 @@ int32_t main()
 
   return 0;
 }
+
+
+// 
