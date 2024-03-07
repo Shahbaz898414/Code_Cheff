2ff3303dcd5ea9018d8f4b4e5c20eb6b8e8d64ce
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-07 14:07:50 +0530

    1

--- Pratice/zzz1.cpp
+++ Pratice/zzz1.cpp
@@ -26,26 +26,4 @@ signed main()
 }
 
 
-/*
 
-Dear Hiring Manager,
-
-I hope this email finds you well. My name is Shahbaz Khan, and I am writing to express my interest in the Frontend Development position at Simtrak Solutions. After carefully reviewing the job description and learning more about your company's innovative projects, I am excited about the prospect of contributing my skills and enthusiasm to your team.
-
-I am currently pursuing a Bachelor's degree in Electronics and Computer Science at Shree LR Tiwari College, Mumbai University, and have gained practical experience through my MERN internship at NULL CLASS W. During this internship, I successfully contributed to frontend development tasks, including creating responsive web pages and implementing user interface features using React.js.
-
-Enclosed is my resume, providing further details about my academic background, technical skills, and relevant projects. In my cover letter, I would like to highlight my passion for frontend development, strong problem-solving abilities, and my commitment to delivering high-quality user experiences.
-
-I am particularly drawn to Simtrak Solutions because of its reputation for fostering creativity and pushing the boundaries of technology. I am confident that my skills in HTML, CSS, and JavaScript, coupled with my hands-on experience with React.js, make me a strong fit for your team.
-
-Thank you for considering my application. I am eager to contribute my skills to Simtrak Solutions and would welcome the opportunity to discuss how my background aligns with your team's needs.
-
-I look forward to the possibility of interviewing with you. Please find my contact information below for any further communication.
-
-Best Regards,
-
-Shahbaz Khan
-7710968756
-
-
-*/
