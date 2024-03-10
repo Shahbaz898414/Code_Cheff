170b062709fbf64ea67cf2881913bc1fd790a36d
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-10 19:48:46 +0530

    1

--- Pratice/Chef And Special Dishes.cpp
+++ Pratice/Chef And Special Dishes.cpp
@@ -18,23 +18,33 @@ int main()
             if (s.substr(0,n/2)==s.substr(n/2,n/2)) cout << "YES" << endl;
             else cout << "NO" << endl;
         }else {
-           int p1 = 0; int p2 = n/2+1;
+
+
+            int p1 = 0; int p2 = n/2+1;
             string temp1 = "";
             string temp2 = "";
             
+
             while (p1<n/2+1 and p2<n) {
                 if (s[p1]!=s[p2]) p1++;
                 else temp1+=s[p1], p1++, p2++;
             }
+
+
             p1 = 0; p2 = n/2;
+
+
             while (p1<n/2 and p2<n) {
                 if (s[p1]!=s[p2]) p2++;
                 else temp2+=s[p1], p1++, p2++;
             }
+
+
             if (temp1==s.substr(n/2+1, n/2) or temp2==s.substr(0,n/2)) {
                 cout << "YES"<< endl;
             }
-            else cout << "NO" << endl;
+            else 
+               cout << "NO" << endl;
         }
 
 
