bddfb66694d0ba8fb5aecb2fbc1e7811f09592b0
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-18 16:44:56 +0530

    1

--- Pratice/Doraemon.cpp
+++ Pratice/Doraemon.cpp
@@ -6,21 +6,26 @@ using namespace std;
 const int mod = 1000000007;
 const int MOD = 1000000007;
 
-
-
 int main()
 {
 
-  int t;cin >> t;
+  int t;
+  cin >> t;
 
-  while (t--) {
-     ll n,m;cin>>n>>m;
-    
+  while (t--)
+  {
+    ll n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> v(n, vector<int>(m, 0));
+    for (int i = 0; i < n; i++)
+    {
+      for (int j = 0; j < m; j++)
+      {
+        cin >> v[i][j];
+      }
+    }
   }
-
- 
 
   return 0;
 }
-
-
