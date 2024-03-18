eab3c075ab2f02213e5b11c82930d292ba3113e2
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-18 15:02:37 +0530

    1

--- Pratice/Doraemon.cpp
+++ Pratice/Doraemon.cpp
@@ -6,15 +6,7 @@ using namespace std;
 const int mod = 1000000007;
 const int MOD = 1000000007;
 
-int powerOfTwo(int n)
-{
-  int result = 1;
-  for (int i = 0; i < n; i++)
-  {
-    result = (result * 2) % MOD;
-  }
-  return result;
-}
+
 
 int main()
 {
