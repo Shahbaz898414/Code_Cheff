bbb95fc777a68dfdebed259e8997c217632b5a50
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-18 17:13:19 +0530

    1

--- Pratice/Doraemon.cpp
+++ Pratice/Doraemon.cpp
@@ -17,7 +17,7 @@ int main()
     ll n, m;
     cin >> n >> m;
 
-    vector<vector<int>> v(n, vector<int>(m, 0));
+    vector<vector<ll>> v(n, vector<ll>(m, 0));
     for (int i = 0; i < n; i++)
     {
       for (int j = 0; j < m; j++)
@@ -25,6 +25,26 @@ int main()
         cin >> v[i][j];
       }
     }
+
+    ll ans = 0;
+    for (ll i = 0; i < n; i++)
+    {
+      for (ll j = 0; j < m; j++)
+      {
+        int range = min(min(m - j - 1, j), min(n - i - 1, i));
+        for (int k = 0; k <= range; k++)
+        {
+          if (v[i - k][j] == v[i + k][j] and v[i][j - k] == v[i][j + k])
+          {
+            ans++;
+          }
+          else
+            break;
+        }
+      }
+    }
+
+    
   }
 
   return 0;
