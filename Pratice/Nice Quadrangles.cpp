1a40840cdc881fb02fa7a1f82e2791933f65bf15
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-19 16:41:16 +0530

    1

---
+++ Pratice/Nice Quadrangles.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+const int mod = 1000000007;
+const int MOD = 1000000007;
+
+int main()
+{
+
+  int t;
+  cin >> t;
+
+  while (t--)
+  {
+
+    ll n;
+    cin >> n;
+  }
+
+  return 0;
+}
