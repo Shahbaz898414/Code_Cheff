cd939c53c7f82bda0261623f10cc45006a972c78
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-04-06 13:56:14 +0530

    1

--- Pratice/test09.cpp
+++ Pratice/test09.cpp
@@ -1,20 +1,53 @@
-#include<bits/stdc++.h>
-#define int long long
+#include <iostream>
+#include <algorithm>
+
 using namespace std;
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+         if(root==NULL) return 0;
+
+         int l=maxDepth(root->left);
+         int r=maxDepth(root->right);
+
+
+         return max(l,r)+1;
+    }
+};
+
 int main() {
-  
-   int n,x=0,y;cin>>n>>y;
- 
-   int a[n];
+    // Construct the binary tree
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(9);
+    root->right = new TreeNode(20);
+    root->right->left = new TreeNode(15);
+    root->right->right = new TreeNode(7);
 
-   for (int i=0; i<n; i++) {
-      cin>>a[i];
+    // Create an instance of the Solution class
+    Solution solution;
 
-   }
-   
-   
+    // Calculate the maximum depth of the binary tree
+    int depth = solution.maxDepth(root);
 
- 
+    // Display the maximum depth of the binary tree
+    cout << "Maximum Depth of Binary Tree: " << depth << endl;
+
+    // Free the memory allocated for the binary tree
+    delete root->left;
+    delete root->right->left;
+    delete root->right->right;
+    delete root->right;
+    delete root;
+
+    return 0;
 }
-
