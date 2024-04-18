b794cd934877cf46550af1ebc2ce641709012a57
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-04-18 19:02:41 +0530

    1

--- Pratice/test09.cpp
+++ Pratice/test09.cpp
@@ -51,3 +51,62 @@ int main() {
 
     return 0;
 }
+
+/*
+
+
+PROJECTS FAIR  | JavaScript, React Js, HTML,CSS ,FireBase 2021-22
+• Created a platform for students to showcase their projects. Implemented Firebase Authentication for secure login and
+registration.
+• Integrated Firebase Real time Database for scalable and reliable backend functionality. Utilized the database to store and
+retrieve project data.
+• Improved overall functionality and usability by implementing features such as project submission.
+
+
+Halo Chat App W | Next.js, Tailwind Css, MongoDB, Next Auth, Pusher 2024
+• Users can easily create personalized accounts, providing them with a unique identity within the app.
+• Developed Halo Chat App for real-time messaging, enabling swift and efficient message exchange.
+• Implemented custom group creation feature, facilitating communication with multiple individuals simultaneously.
+• Halo Chat App offers users the ability to update their profile image and name, ensuring their online presence remains current and reflective of their identity.
+
+
+ImageKit.io | typescript, javascript, MongoDB,Stripe 2024
+• Developed sign-in, sign-up, and email confirmation with OTP capabilities.
+
+• Enhanced image editing capabilities by implementing object removal functionality and enabling users to revert edited images to their original state.
+• Empowered users to modify colors of specific objects within images.
+• Developed generative fill functionality for images, providing users with automatic area filling using generated content.
+• Enhanced image editing capabilities and versatility by enabling users to easily remove backgrounds from images.
+
+
+
+
+Global Rank 65 in CodeChef Jan Starters 87, 2023.
+Global Rank 68 in CodeChef Mar Starters 80, 2023.
+Ranked under 58 at Codechef which ranks among the top sites for competitive programmers. Highestrating of 1994 and rated 4.
+Global Rank 323 in Codeforces Round 902 (Div. 2).
+Solved more than 1500+ problems across platforms.
+Microsoft  Al Classroom series.
+Knight at leetcode
+Student Ambassador of internshala from Shree LR Tiwari College.
+
+
+
+Register yourself and put your name here only ... registration is compulsory for all. Register
+yourself and put your name here:
+1. Nilesh Vishwakarma
+2. Adarsh dubey
+3. Chinmay Ghadigaonkar
+4. Pradeep Choudhary
+5. Nidhi Brahmgotia
+6. Vrunda Tawde
+7. Neel Acharya
+8. Aniket Chauhan
+9. Arun Bhandari
+10.Himanshu Rai
+11.Varun Mahajan
+12.Leelasa Thakur
+13. Aman Tripathi
+14. Shahbaz Khan
+
+*/
