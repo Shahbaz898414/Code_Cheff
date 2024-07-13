8c5f3c5c16bda362661d95fef89629f0f53fa1fa
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-07-13 14:17:49 +0530

    13-07-24

---
+++ Pratice/Make Permutation.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using P = pair<ll, ll>;
+
+vector<vector<P>> graph;
+
+
+
+signed main() {
+  ll t;cin>>t;
+
+  while(t--){
+    
+    ll n;cin>>n;
+
+    vector<ll>  arr(n);
+
+    for (ll i = 0; i < n; i++) {
+
+      cin>>arr[i];
+    }
+    
+  }
+
+
+    return 0;
+}
+
--- Pratice/tets.cpp
+++ Pratice/tets.cpp
@@ -1,92 +1,49 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
-using namespace __gnu_pbds;
 
-template <class T>
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+using ll = long long;
+using P = pair<ll, ll>;
 
-typedef pair<int, int> Pair;
-typedef vector<Pair> VectorPair;
-typedef vector<int> VectorInt;
-typedef vector<VectorInt> VectorVectorInt;
+vector<vector<P>> graph;
 
-const long long MOD = 1e9 + 7;
-
-template <typename T>
-struct SegmentTree {
-    T unit;
-    T (*func)(T obj1, T obj2);
-    vector<T> s;
-    int n;
-    SegmentTree(int n, T (*f)(T obj1, T obj2), T def) : s(2 * n, def), n(n), func(f), unit(def) {}
-    void update(int pos, T val) {
-        for (s[pos += n] = val; pos /= 2;)
-            s[pos] = func(s[pos * 2], s[pos * 2 + 1]);
-    }
-    T query(int b, int e) {
-        e++;
-        T ra = unit, rb = unit;
-        for (b += n, e += n; b < e; b /= 2, e /= 2) {
-            if (b % 2)
-                ra = func(ra, s[b++]);
-            if (e % 2)
-                rb = func(s[--e], rb);
-        }
-        return func(ra, rb);
-    }
-};
-
-int join(int a, int b) { return max(a, b); }
-int sum(int a, int b) { return (a + b); }
-
-void solve() {
-    int numElements;
-    cin >> numElements;
-    vector<int> vectorA(numElements);
-    int limit;
-    cin >> limit;
-    VectorInt vectorB(numElements);
-    for (int i = 0; i < numElements; i++) {
-        cin >> vectorA.at(i);
-    }
-    for (int i = 0; i < numElements; i++) {
-        cin >> vectorB[i];
-    }
-    swap(vectorA, vectorB);
-    int sumH = 0;
-    SegmentTree<int> segmentTree(numElements, join, 0);
-    SegmentTree<int> sumSegmentTree(numElements, sum, 0);
-
-    for (int i = 0; i < numElements; i++) {
-        sumH += vectorA[i];
-        segmentTree.update(i, sumH - vectorB[i]);
-        sumSegmentTree.update(i, vectorA[i]);
-    }
-
-    for (int i = 0; i < numElements; i++) {
-        int goneTill = upper_bound(vectorB.begin(), vectorB.end(), vectorB[i] + 2 * limit) - vectorB.begin() - 1;
-        int worst = 0;
-        if (i)
-            worst = max(worst, segmentTree.query(0, i - 1));
-        if (goneTill != numElements - 1)
-            worst = max(worst, segmentTree.query(goneTill + 1, numElements - 1) - sumSegmentTree.query(i, goneTill));
-        if (worst == 0) {
-            cout << "YES\n";
-            return;
+P dfs(ll v, ll d, ll p) {
+    P res = {d, v};
+    for (auto  it : graph[v]) {
+        if (it.first != p) {
+            res = max(res, dfs(it.first, d + it.second, v));
         }
     }
-    cout << "NO\n";
+    return res;
 }
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int totalTests = 1;
-    cin >> totalTests;
-    for (int testNo = 1; testNo <= totalTests; testNo++) {
-        solve();
+signed main() {
+    ll n;
+    cin >> n;
+    
+    graph.resize(n);
+    ll total_weight = 0;
+    
+    for (ll i = 0; i < n - 1; ++i) {
+        ll A, B, C;
+        cin >> A >> B >> C;
+        A--; B--;
+        graph[A].emplace_back(B, C);
+        graph[B].emplace_back(A, C);
+        total_weight += 2 * C; 
     }
+    
+    
+    P first_dfs = dfs(0, 0, -1);
+    ll farthest_node = first_dfs.second;
+    
+
+    P second_dfs = dfs(farthest_node, 0, -1);
+    ll diameter = second_dfs.first;
+    
+  
+    ll min_travel_distance = total_weight - diameter;
+    cout << min_travel_distance << endl;
+
+
     return 0;
 }
